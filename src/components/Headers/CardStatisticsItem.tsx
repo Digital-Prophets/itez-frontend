700f992d10db7d97589cecd9c9a1d2f9a16c9729
Mathewsmusukuma <sikaili99@gmail.com>
2021-11-16 23:00:10 +0200

    Added new dasboard cards changes

--- src/components/Headers/CardStatisticsItem.tsx
+++ src/components/Headers/CardStatisticsItem.tsx
@@ -2,26 +2,32 @@ import React from 'react';
 
 type IStatistics = {
   title: string;
-  Icon: React.ComponentType<any>;
+  color: string;
+  Icon: any;
   value: number;
 };
 
 export default function CardStatisticsItem({
   title,
+  color,
   Icon,
   value,
 }: IStatistics) {
   return (
     <>
-      <div className="transform hover:scale-105 cursor-pointer transition delay-100 w-3/12  p-2 py-4 shadow-xl  border rounded-xl bg-gradient-to-r">
-        <div className="flex justify-between">
-          <div></div>
-          <div className=" w-10  h-10 flex items-center justify-center  bg-gray-300 rounded-xl m-1  bg-opacity-30">
-            {Icon}
+      <div className="bg-white shadow-xl transform hover:scale-105 cursor-pointer transition delay-100 pb-8 pt-2 mt--4">
+        <div className="flex m-2">
+          <div className="flex-grow">
+            <p className="text-lg text-gray-700 font-bold mb-2">{title}</p>
+          </div>
+          <div className="flex-none">
+            <Icon className="w-10 h-10" style={{ color }} />
           </div>
         </div>
-        <p className="text-black text-md  ">{title}</p>
-        <p className="text-black text-lg  font-semibold  ">{value}</p>
+
+        <div className="text-center">
+          <p className="text-base text-gray-800 font-bold">{value}</p>
+        </div>
       </div>
     </>
   );
