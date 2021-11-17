026f56415253c1d4203421520a0f1c17f4f4849e
Jachin Manda <jachin.dev@outlook.com>
2021-11-17 16:29:42 +0200

    Add work details listing

---
+++ src/pages/occupation/index.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+
+import { NextPage } from 'next';
+
+import Pagination from '../../components/Pagination';
+import { config } from  '../../config';
+import { Props } from "../../types/workDetail"
+
+const WorkDetails: NextPage<Props> = ({ data }) => {
+  const { results } = data;
+  return (
+    <>
+      <div className="flex justify-end">
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-5 mr-8 rounded">
+          Add Work Details
+        </button>
+      </div>
+      <div className="main-content flex flex-col flex-grow p-4">
+        <h1 className="font-bold text-2xl text-gray-700 mb-3">Work Details</h1>
+        <div className="flex flex-col">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Gross Pay
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Company
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Company Address
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Is company insured?
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {results.map((work_detail) => (
+                      <tr key={work_detail.id}>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-500">
+                            {work_detail.gross_pay}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap">
+                          <div className="text-sm text-gray-500">
+                            {work_detail.company}
+                          </div>
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          {work_detail.work_address}
+                        </td>
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          {work_detail.insured ? <p>Yes</p> : <p>No</p>}
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <Pagination />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export async function getStaticProps() {
+  const BASE_URL = config.ITEZ_API_URI;
+  const res = await fetch(`${BASE_URL}/work_detail`);
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+}
+
+export default WorkDetails;
