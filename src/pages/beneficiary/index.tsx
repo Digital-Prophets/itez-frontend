df8f9bcc341f8b0a922fa0a5ab379df3f84a2f9a
Jachin Manda <jachin.dev@outlook.com>
2021-11-17 22:31:03 +0200

    Update beneficiary table to SWR for data fetching

--- src/pages/beneficiary/index.tsx
+++ src/pages/beneficiary/index.tsx
@@ -1,11 +1,29 @@
 import { NextPage } from 'next';
+import useSWR from 'swr';
 
+import Spinner from '../../components/Spinner';
 import Pagination from '../../components/Pagination';
 import { config } from '../../config';
-import { Props } from '../../types/beneficiary';
+import BeneficiaryTable from '../../components/Beneficiary/BeneficiaryTable';
 
-const BeneficiaryTable: NextPage<Props> = ({ data }) => {
-  const { results } = data;
+
+const dataFetcher = (url: string) => fetch(url).then(res => res.json())
+
+
+function fetchBeneficiaryData () {
+  const BASE_URL = config.ITEZ_API_URI;
+  const { data, error } = useSWR(`${BASE_URL}/beneficiaries`, dataFetcher)
+
+  return {
+    data: data,
+    isLoading: !error && !data,
+    isError: error
+  }
+}
+
+const BeneficiaryList: NextPage = () => {
+  const {data, isLoading} = fetchBeneficiaryData();
+
   return (
     <>
       <div className="flex justify-end">
@@ -19,96 +37,15 @@ const BeneficiaryTable: NextPage<Props> = ({ data }) => {
           <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
               <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                <table className="min-w-full divide-y divide-gray-200">
-                  <thead className="bg-gray-50">
-                    <tr>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Full Name
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Birthdate
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Gender
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Marital Status
-                      </th>
-                      <th
-                        scope="col"
-                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
-                      >
-                        Phone Number
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody className="bg-white divide-y divide-gray-200">
-                    {results.map((person) => (
-                      <tr key={person.email}>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="flex items-center">
-                            <div className="ml-4">
-                              <div className="text-sm font-medium text-gray-900">
-                                {person.first_name} {person.last_name}
-                              </div>
-                              <div className="text-sm text-gray-500">
-                                {person.email}
-                              </div>
-                            </div>
-                          </div>
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="text-sm text-gray-500">
-                            {person.date_of_birth}
-                          </div>
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap">
-                          <div className="text-sm text-gray-500">
-                            {person.gender}
-                          </div>
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {person.marital_status}
-                        </td>
-                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                          {person.phone_number}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
+              {isLoading ? <Spinner /> : data ? <BeneficiaryTable data={data}/> : <h2 className="font-bold">An Error occured while fetching data.</h2>}
+                <Pagination />
               </div>
             </div>
           </div>
-          <Pagination />
         </div>
       </div>
     </>
   );
 };
 
-export async function getStaticProps() {
-  const BASE_URL = config.ITEZ_API_URI;
-  const res = await fetch(`${BASE_URL}/beneficiaries`);
-  const data = await res.json();
-
-  return {
-    props: {
-      data,
-    },
-  };
-}
-
-export default BeneficiaryTable;
+export default BeneficiaryList;
